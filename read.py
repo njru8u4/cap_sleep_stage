4831026e483d4ac8fc85974df3ca676d7cec031e
njru8u4 <32103433+njru8u4@users.noreply.github.com>
2020-12-19 19:16:27 +0900

    76%

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import convolve, find_peaks
 import time
+import shutil
 
 def stage_trans(stage_name):
     if stage_name == 'W' or stage_name == 'Wake' or stage_name == 'w' or stage_name == 'w':
@@ -23,10 +24,22 @@ def stage_trans(stage_name):
         write_error(f"{stage_name}\t unknown stage name\n")
         return 6
    
-def write_error(massage):
+def write_error(message):
     f = open('./datas/errors.txt', 'a')
-    f.write(massage)
+    f.write(message)
     f.close()
+
+def write_info(message):
+    f = open('./file_info.txt', 'a')
+    f.write(message)
+    f.close()
+
+def save_pic(sig, timeStamp, file_name, size):
+    temp = file_name.split('/')
+    plt.plot(sig)
+    plt.title(f"peaks = {size}")
+    plt.savefig(f"./pictures/{temp[-1]}_at_{timeStamp}.png")
+    plt.close()
 
 def get_timeStamp(str):
     try:
@@ -38,7 +51,11 @@ def get_timeStamp(str):
 
 def read_PPG_from_edf(file_path, times, stage, interval_time):
     data = mne.io.read_raw_edf(file_path)
-    sample_f = int(data.info['sfreq'])
+    if data.info['sfreq'].is_integer():
+        sample_f = int(data.info['sfreq'])
+    else:
+        write_error(f"{file_path}\t sample rate not integer\n")
+        return 1, 1
 
     #raw, times = data.get_data(picks='ECG1-ECG2', return_times=True)
     try:
@@ -49,6 +66,17 @@ def read_PPG_from_edf(file_path, times, stage, interval_time):
         except:
             write_error(f"{file_path}\t pleth load error\n")
             return 1, 1
+
+    #### dealing time ##########
+    #timeArray = time.strptime(data.info['meas_date'], "%Y-%m-%d %H:%M:%S+00:00")
+    #start_time = timeArray.tm_hour*3600 + timeArray.tm_min*60 + timeArray.tm_sec
+    start_time = data.info['meas_date'].hour * 3600 + data.info['meas_date'].minute * 60 + data.info['meas_date'].second
+    for i in range(len(times)):
+        if times[i] >= start_time:
+            times[i] = times[i] - start_time
+        else:
+            times[i] = 24*3600 - start_time + times[i]
+
 
     raw = raw.reshape(len(raw[0]))
     raw = -raw
@@ -62,10 +90,14 @@ def read_PPG_from_edf(file_path, times, stage, interval_time):
         interval = np.zeros(100*int(interval_time / 30))
         start = int(times[i] - (interval_time - 30) / 2) * sample_f
         end = int(times[i] + 30 + (interval_time - 30) / 2) * sample_f
-        peaks, _ = find_peaks(raw[start : end], distance= int(sample_f / 3), width= int(sample_f / 6))
+        if end > len(raw):
+            write_error(f"{file_path}\t time overflow, rest epoch {len(times) - i}\n")
+            break
+        peaks, _ = find_peaks(raw[start : end], distance= int(sample_f / 3), width= int(sample_f / 8))
         
         if peaks.size < 15*interval_time/30 or peaks.size > 100*interval_time/30:
             write_error(f"{file_path}\t have peak error at time stamp: {times[i]}\n")
+            save_pic(raw[start: end], times[i], file_path, peaks.size)
             continue
         peak2sec = peaks/sample_f*1000
         real_peak = peak2sec[1:] - peak2sec[0:-1]
@@ -83,6 +115,7 @@ def read_stage(file_path, interval_time):
     style = 0
     temp_stage = []
     temp_time = []
+
     start_time = 0
     for i in range(len(lines)):
         if i < start_index:
@@ -127,17 +160,20 @@ def read_stage(file_path, interval_time):
         write_error(f"{file_path}\t load txt error\n")
         return 1, 1
 
-    for i in range(len(times)):
-        if times[i] >= start_time:
-            times[i] = times[i] - start_time
-        else:
-            times[i] = 24*3600 - start_time + times[i]
+    
 
     return temp_stage, times
 
 def read_files(interval_time):
     if os.path.exists("./datas/errors.txt"):
         os.remove("./datas/errors.txt")
+    if os.path.exists('./file_info.txt'):
+        os.remove('./file_info.txt')
+    if os.path.exists("./pictures/"):
+        shutil.rmtree('./pictures')
+        time.sleep(1)
+        
+    os.mkdir('./pictures/')
     record = open('./cap-sleep-database-1.0.0/RECORDS', 'r')
     lines = record.readlines()
     all_stages = []
@@ -148,10 +184,10 @@ def read_files(interval_time):
         txt_file = './cap-sleep-database-1.0.0/' + temp[0] + '.txt'
         edf_file = './cap-sleep-database-1.0.0/' + temp[0] + '.edf'
         
-        stage, time = read_stage(txt_file, interval_time)
-        if stage == 1 and time == 1:
+        stage, times = read_stage(txt_file, interval_time)
+        if stage == 1 and times == 1:
             continue
-        PPG, stage = read_PPG_from_edf(edf_file, time, stage, interval_time)
+        PPG, stage = read_PPG_from_edf(edf_file, times, stage, interval_time)
         if stage == 1 and PPG == 1:
             continue
 
@@ -171,19 +207,47 @@ def read_files(interval_time):
 
 def read_test(name):
     data = mne.io.read_raw_edf(name)
+    sample_f = int (data.info['sfreq'])
+    raw = data.get_data(picks='PLETH')
+    raw = raw.reshape(len(raw[0]))
+    
+    #plt.plot(raw[600*sample_f:610*sample_f])
+    #plt.show()
+    
+    
+    
+    raw = -raw
+    #plt.plot(raw[600*sample_f:610*sample_f])
+    #plt.show()
+    
+
+    mid_filter_num = int(sample_f / 25 / 2) + 1
+    filt = np.array([1/mid_filter_num]*mid_filter_num)
+    raw = convolve(raw, filt)
+    plt.plot(raw[60*sample_f:70*sample_f])
+    plt.show()
+    
+
+
+    """
     print (data.info)
     print (data.info['sfreq'])
-    print (data.info['ch_names'])
+    print (data.info['meas_date'])
+    print (data.info['meas_date'].second)
+    print (data.info['meas_date'].minute)
+    """
+
 
 if __name__ == "__main__":
-    #read_test("./cap-sleep-database-1.0.0/sdb3.edf")
+    read_test("./cap-sleep-database-1.0.0/plm10.edf")
     
     #stage, time = read_stage("./cap-sleep-database-1.0.0/n16.txt", 150)
     #if stage == 1 and time == 1:
     #    print ("fail")
     #PPG, stage = read_PPG_from_edf("./cap-sleep-database-1.0.0/n14.edf", time, stage)
     
-    interval_time = 150
-    PPG, stage = read_files(interval_time)
-    np.save('./datas/cap_PPG_150s_initial', PPG)
-    np.save('./datas/cap_label_for_PPG_150s_initial', stage)
+    #interval_time = 30
+    #PPG, stage = read_files(interval_time)
+    #np.save('./datas/cap_PPG_30s', PPG)
+    #np.save('./datas/cap_label_for_PPG_30s', stage)
+    
--- train_by_unbalance.py
+++ train_by_unbalance.py
@@ -2,14 +2,17 @@ import numpy as np
 from keras.layers import Conv1D  # Convolution Operation
 from keras.layers import MaxPooling1D, AveragePooling1D
 from keras.layers import Flatten, BatchNormalization
-from keras.layers import Dense, Dropout
-from keras.layers import Input, concatenate
+from keras.layers import Dense, Dropout, GlobalAveragePooling1D
+from keras.layers import Input, concatenate, Activation
 from keras.optimizers import Adam, SGD
 from keras import Model
 from keras.callbacks import EarlyStopping
+from keras.utils import plot_model
+
 from sklearn.model_selection import train_test_split
 import time
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 import preprocess
 
@@ -63,23 +66,33 @@ def inter_model(input1, pool_type):
         return pooling
 
     x = input1
+    """
+    cell1 = cell(16, x, pool_type)
+    cell1 = Dropout(0.2)(cell1)
+    cell1 = cell(16, cell1, pool_type)
+    cell1 = Dropout(0.2)(cell1)
+    cell1 = cell(16, cell1, pool_type)
+    cell1 = Dropout(0.2)(cell1)
+    cell1 = cell(16, cell1, pool_type)
+    """
     cell1 = cell(16, x, 0)
     cell1 = cell(16, cell1, pool_type)
-    cell1 = Dropout(0.1)(cell1)
+    cell1 = Dropout(0.2)(cell1)
     cell1 = cell(16, cell1, 0)
     cell1 = cell(16, cell1, pool_type)
-    cell1 = Dropout(0.1)(cell1)
+    cell1 = Dropout(0.2)(cell1)
     cell1 = cell(16, cell1, 0)
     cell1 = cell(16, cell1, pool_type)
-    cell1 = Dropout(0.1)(cell1)
+    cell1 = Dropout(0.2)(cell1)
     cell1 = cell(16, cell1, 0)
     cell1 = cell(16, cell1, pool_type)
+    
     #cell1 = Dropout(0.3)(cell1)
     #cell1 = cell(32, cell1, 0)
     #cell1 = cell(32, cell1, pool_type)
     #cell1 = Dropout(0.2)(cell1)
     
-    cell1 = Dropout(0.5)(cell1)
+    #cell1 = Dropout(0.5)(cell1)
     
     return cell1
 
@@ -88,34 +101,64 @@ def get_model(input_data, label, classes):
     M = inter_model(input1, 2)
     M2 = inter_model(input1, 1)
     MM = concatenate([M, M2], axis=2)
-    flat = Flatten()(M2)
-    D = Dense(units=64, activation="relu", name="dense_out")(flat)
+    #flat = Flatten()(MM)
+    #flat = Dropout(0.3)(flat)
+    #D = Dense(units=64, activation="relu", name="dense_out")(flat)
+
+    #D = Conv1D(4, kernel_size = 2, activation = "relu")(M2)
+    #D_out = GlobalAveragePooling1D()(D)
+    #D_out = Activation('softmax', name = "output_layer")(D_out)
+    D = GlobalAveragePooling1D()(M2)
+    #D = Dropout(0.2)(D)
     D_out = Dense(units=classes, activation="softmax", name="output_layer")(D)
 
     model = Model(inputs= input1, outputs= D_out)
-    model.compile(optimizer = Adam(lr = 0.001), loss = 'categorical_crossentropy', metrics= ['categorical_accuracy'])
+    model.compile(optimizer = Adam(lr = 0.0005), loss = 'categorical_crossentropy', metrics= ['categorical_accuracy'])
     model.summary()
+    #plot_model(model, to_file='./model.png')
+    
     early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=2)
-    model.fit(input_data, label, epochs=200, validation_split=0.3, callbacks=[early_stopping], batch_size=64)
+    history = model.fit(input_data, label, epochs=200, validation_split=0.3, callbacks=[early_stopping], batch_size=128, shuffle=True)
+
+    
+
+    plt.plot(history.history['categorical_accuracy'])
+    plt.plot(history.history['val_categorical_accuracy'])
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.savefig("./history_acc.png")
+    plt.close()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.ylabel('Loss')
+    plt.xlabel('epoch')
+    plt.savefig("./history_loss.png")
+    plt.close()
+
+    
     return model
 
 def result(train, label, classes, model_name = None):
     x_train1, x_test1, y_train1, y_test1 = train_test_split(train, label, test_size = 0.2, shuffle = True)
     model = get_model(x_train1, y_train1, classes)
 
-    if model_name != None:
-        model.save("./models"+model_name+".h5")
     
     pred = model.predict(x_test1)
+    (loss, acc) = model.evaluate(x_test1, y_test1)
+    if model_name != None:
+        model.save(f"./models/{model_name}_acc_{acc}.h5")
+    
     results = analysis(y_test1, pred, classes)
+
     return results
 
 if __name__ == "__main__":
-    inter = np.load("./datas/cap_PPG_150s_initial.npy")
-    label = np.load("./datas/cap_label_for_PPG_150s_initial.npy")
+    inter = np.load("./datas/cap_PPG_150s.npy")
+    label = np.load("./datas/cap_label_for_PPG_150s.npy")
     classes = 4
     train, stage = preprocess.get_data_balance(inter, label, classes)
-    results = result(train, stage, classes)
+    results = result(train, stage, classes, "tt")
     print(results)
 
 
